0b57bce1045d873656b81ecb479442d227aa7ab6
Tristan Carter <tristancarter2772@gmail.com>
2026-01-08 08:53:52 -0600

    :+1: setup tailwindcss, daisyUI, icons, and set light/dark mode themes (#52)

--- app/components/title-navbar.vue
+++ app/components/title-navbar.vue
@@ -1,3 +1,9 @@
 <template>
-  hello world!!!!!!!!!!!!!!
+  <h1 class="text-lg">
+    hello worldly things!!
+  </h1>
+
+  <button class="btn btn-primary">
+    View Jars <Icon name="tabler:tip-jar" size="24" />
+  </button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/hints", "@nuxt/icon"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
+  css: ["~/assets/css/main.css"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
