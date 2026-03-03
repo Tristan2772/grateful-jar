8bcf25f7f8eed148579443140402aa4c30a36e60
Tristan <tristancarter2772@gmail.com>
2026-03-03 08:45:05 -0600

    :sparkles: Created landing page

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import AppTitleNavbar from "./components/app/title-navbar.vue";
+</script>
+
 <template>
-  <NuxtPage />
+  <div class="flex flex-col h-screen">
+    <AppTitleNavbar class="flex-shrink-0" />
+    <NuxtPage class="flex-1 overflow-hidden" />
+  </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <TitleNavbar />
+  <div class="hero bg-base-200 h-full">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Grateful Jar
+        </h1>
+        <p class="py-6">
+          Write out what you are grateful for and collect those memories in grateful jars. Come back to remind yourself that there is plenty to be grateful for!
+        </p>
+        <button class="btn btn-primary min-w-50">
+          View Jars
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
