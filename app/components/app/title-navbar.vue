550a1fb94991ec1a79ed599bb6fe1580e1264427
Tristan Carter <tristancarter2772@gmail.com>
2026-03-03 08:49:06 -0600

    ❇️ created landing page (#53)
    * Created landing page
    
    * syntax edit

---
+++ app/components/app/title-navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <NuxtLink class="btn btn-ghost text-lg" to="/">
+        Grateful Jar
+      </NuxtLink>
+    </div>
+    <div class="flex-none">
+      <AppAuthButton />
+    </div>
+  </div>
+</template>
--- app/components/title-navbar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1 class="text-lg">
-    hello worldly things!!
-  </h1>
-
-  <button class="btn btn-primary">
-    View Jars <Icon name="tabler:tip-jar" size="24" />
-  </button>
-</template>
