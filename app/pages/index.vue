0cb4fc78c7a037f7f7af9265f18c19bf381845f4
Tristan Carter <tristancarter2772@gmail.com>
2026-03-12 15:19:46 -0500

    :sparkles: protected routes redirect if not logged in (#60)

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 h-full container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,10 @@
         <p class="py-6">
           Write out what you are grateful for and collect those memories in grateful jars. Come back to remind yourself that there is plenty to be grateful for!
         </p>
-        <AppAuthButton />
+        <AppAuthButton v-if="!authStore.user" />
+        <NuxtLink v-if="authStore.user" to="/dashboard" class="btn btn-primary">
+          View Jars
+        </NuxtLink>
       </div>
     </div>
   </div>
