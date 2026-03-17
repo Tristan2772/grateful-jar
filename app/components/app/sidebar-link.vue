7a904a17639f0dc1f045ef42d603032fd4a58dda
Tristan <tristancarter2772@gmail.com>
2026-03-16 22:31:57 -0500

    :sparkles: created sidebar, added links, added collapsible functionality

---
+++ app/components/app/sidebar-link.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  link: string;
+  showLabel: boolean;
+}>();
+const route = useRoute();
+</script>
+
+<template>
+  <div class="tooltip-right" :data-tip="showLabel ? undefined : props.label" :class="{ tooltip: !showLabel }">
+    <NuxtLink
+      :to="props.link"
+      class="btn btn-ghost gap-2 p-2 bg-base-100 hover:bg-base-300 w-full"
+      :class="{ 'bg-base-200': route.path === props.link, 'justify-center': !showLabel, 'justify-start': showLabel }"
+    >
+      <slot />
+      <span v-if="props.showLabel">
+        {{ props.label }}
+      </span>
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,3 +1,45 @@
+<script lang="ts" setup>
+const isSidebarOpen = ref(true);
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+}
+</script>
+
 <template>
-  <h1>You are now logged in</h1>
+  <div class="flex-1 flex">
+    <div class="bg-base-100" :class="{ 'w-64': isSidebarOpen, 'w-16': !isSidebarOpen }">
+      <div class="flex hover:bg-base-300 hover:cursor-pointer" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }">
+        <Icon :name="isSidebarOpen ? 'tabler:chevron-left' : 'tabler:chevron-right'" size="42" @click="toggleSidebar" />
+      </div>
+      <div class="flex flex-col">
+        <AppSidebarLink label="My Jars" link="/dashboard" :show-label="isSidebarOpen">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <g
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <path d="M17 4v1.882c0 .685.387 1.312 1 1.618s1 .933 1 1.618V18a3 3 0 0 1-3 3H8a3 3 0 0 1-3-3V9.118c0-.685.387-1.312 1-1.618s1-.933 1-1.618V4M6 4h12z" />
+            </g>
+          </svg>
+        </AppSidebarLink>
+        <AppSidebarLink label="New Jar" link="/dashboard/add" :show-label="isSidebarOpen">
+          <Icon name="tabler:plus" size="24" />
+        </AppSidebarLink>
+        <div class="divider" />
+        <AppSidebarLink label="Sign Out" link="/sign-out" :show-label="isSidebarOpen">
+          <Icon name="tabler:logout-2" size="24" />
+        </AppSidebarLink>
+      </div>
+    </div>
+    <div class="flex-1" />
+  </div>
 </template>
